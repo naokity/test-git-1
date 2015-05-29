3f174dc76ce02a44df414a36525f3af9517dcd82
Kate Lee <hslee.33754@gmail.com>
2015-05-29 14:15:59 -0700

    Add new html lines

--- index.php
+++ index.php
@@ -1,4 +1,50 @@
-<h1>Branch &amp; Merge Group 7</h1>
-<p><small></small></p>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Deserts I like to eat!</title>
+</head>
+
+<body>
+<h1>Branch &amp; Merge Group 7</h1>  
 <h4>This is a random page for you to edit and cause conflicts.</h4>
 
+<ol>
+<li>
+Eclairs
+</li>
+<li>
+Macaroon
+</li>
+<li>
+Mont Blanc
+</li>
+<li>
+French Toast by Aleksandar
+</li>
+<li>
+Creme Burle
+</li>
+<li>
+Chocolate by Ascona in Madison Valley
+</li>
+<li>
+Frans Hot Chocolate
+</li>
+<li>
+Starbucks S'mores Tart
+</li>
+<li>
+Anything from the Culinary Academy @ Seattle CC
+</li>
+<li>
+Japadog Matsury Dog by Shiro - san.
+</li>
+<li>
+Thats it!
+</li>
+</ol>
+<p>Victoria was here</p>
+</body>
+
+</html>
